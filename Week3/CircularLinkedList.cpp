44703805ec162942e75b533d010faa6f2e101a49
unknown <tienvan0158@gmail.com>
2021-11-01 20:25:23 +0700

    complete addTail addhead removeHead removeTail

--- Week3/CircularLinkedList.cpp
+++ Week3/CircularLinkedList.cpp
@@ -3,7 +3,7 @@
 
 struct Node{
     int data;
-    Node* next = NULL;
+    Node* next;
 
     Node(int data){
         this->data = data;
@@ -17,23 +17,48 @@ struct List{
 };
 
 void addHead(List &ls, int data){
+    if (ls.head == ls.tail){
+        Node *newNode = new Node(data);
+        newNode->next = ls.head;
+        ls.head = newNode;
+        ls.tail->next = ls.head;
+        return;
+    }
     ls.tail->next = new Node(data);
     ls.tail->next->next = ls.head;
     ls.head = ls.tail->next;
 }
 
 void addTail(List &ls, int data){
+    if (ls.head == ls.tail){
+        Node* newNode = new Node(data);
+        ls.head->next = newNode;
+        ls.tail = newNode;
+        ls.tail->next = ls.head;
+        return;
+    }
     ls.tail->next = new Node(data);
     ls.tail = ls.tail->next;
     ls.tail->next = ls.head;
 }
 
 void removeHead(List &ls){
+    if (ls.head == ls.tail){
+        ls.head = NULL;
+        ls.tail = NULL;
+        return;
+    }
     ls.head = ls.head->next;
+    ls.tail->next->next = NULL;
     ls.tail->next = ls.head;
 }
 
 void removeTail(List &ls){
+    if (ls.head == ls.tail){
+        ls.head = NULL;
+        ls.tail = NULL;
+        return;
+    }
     Node * cur = ls.head;
     while (cur->next != ls.tail){
         cur = cur->next;
@@ -43,4 +68,5 @@ void removeTail(List &ls){
     ls.tail = cur;
     ls.tail->next = ls.head;
 }
+
 
